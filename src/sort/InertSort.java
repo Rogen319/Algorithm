464289db1a3c65c74ad0f83652e04a7cf871e711
Rogen319 <2391289924@qq.com>
2015-09-11 10:56:03 +0800

    Clean and modify the code.

--- src/sort/InertSort.java
+++ src/sort/InertSort.java
@@ -17,8 +17,6 @@ public class InertSort implements ISort {
 				data[i] = key;
 				i -= 1;
 			}
-
 		}
-
 	}
 }
--- src/sort/TestMain.java
+++ src/sort/TestMain.java
@@ -6,18 +6,17 @@ public class TestMain {
 
 	public static void main(String[] args) throws InstantiationException,
 			IllegalAccessException {
-		// int[] data = DataFactory.SORTDATA;
+		 //int[] data = DataFactory.SORTDATA;
 		double[] double_data = DataFactory.SORTDATADOUBLE;
-		// ISort sort = InertSort.class.newInstance();
-		// ISort sort = Bubblesort.class.newInstance();
-		// ISort sort = Divide_Conquer_Combine.class.newInstance();
+		 //ISort sort = InertSort.class.newInstance();
+		 //ISort sort = Bubblesort.class.newInstance();
 		// ISort sort = HeapSort.class.newInstance();
 		// ISort sort = QuickSort.class.newInstance();
-		// ISort sort = Divide.class.newInstance();
-		// sort.sort(data);
-		// for (int i : data) {
-		// System.out.println(i);
-		// }
+//		 ISort sort = Divide.class.newInstance();
+//		 sort.sort(data);
+//		 for (int i : data) {
+//		 System.out.println(i);
+//		 }
 		// CountSort countSort = new CountSort();
 		// int[] result = countSort.sort(data, 1223);
 		// for (int i : result) {
